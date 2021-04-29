40b884c79ae4bbfc0874fd5ab6dbd88c42f55161
ko1z-se-2015 <201402@astanait.edu.kz>
2021-04-29 11:35:43 +0600

    books

--- DB.php
+++ DB.php
@@ -2,7 +2,7 @@
 $mysqli = false;
 
     global $mysqli;
-    $mysqli = mysqli_connect("localhost", "root", "root","project");
+    $mysqli = mysqli_connect("localhost", "root", "","project");
 
     if($mysqli-> connect_error){
         die("Coonect faile:" .$mysqli->connect_error);
@@ -18,22 +18,6 @@ function closeDB()
     $mysqli->close();
 }
 
-//$user = 'root';
-//$password = 'root';
-//$db = 'user';
-//$host = 'localhost';
-//$port = 3307;
-//
-//$link = mysql_connect(
-//    "$host:$port",
-//    $user,
-//    $password
-//);
-//$db_selected = mysql_select_db(
-//    $db,
-//    $link
-//);
 
-//    global $sql = mysqli_connect("localhost", "root" , "root" , "project");
 //?>
 
