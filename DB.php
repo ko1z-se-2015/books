2a8be5b2eaa488222c6717bd9f4d9d19eb577cae
elhanz <elhanz@mail.ru>
2021-05-04 09:39:19 +0600

    fix

--- DB.php
+++ DB.php
@@ -2,13 +2,13 @@
 $mysqli = false;
 
     global $mysqli;
-    $mysqli = mysqli_connect("localhost", "root", "root","project");
+    $mysqli = mysqli_connect("localhost", "root", "","project");
 
     if($mysqli-> connect_error){
         die("Coonect faile:" .$mysqli->connect_error);
         echo "NETNETNET";
     }
-    echo "DDADADADADADADADADa";
+    echo "vrode pashet";
     $mysqli->query("SET NAMES 'utf-8'");
 
 
