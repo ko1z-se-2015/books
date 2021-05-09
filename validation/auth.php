6857fcd9b8bcd81256cf1c83955cadcc75d56117
elhanz <elhanz@mail.ru>
2021-05-09 23:06:54 +0600

    reg_log

---
+++ validation/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+$name=filter_var(trim($_POST['nickname2']),
+    FILTER_SANITIZE_STRING);
+$pass=filter_var(trim($_POST['pass2']),
+    FILTER_SANITIZE_STRING);
+
+$pass=md5($pass."imperium4");
+
+$mysql=new mysqli('localhost','root','','project');
+
+$result=$mysql->query("Select * from `users` where `nickname`='$name' and `pass`=   '$pass'  ");
+$user=$result->fetch_assoc();
+if(count((array)$user)==0){
+    echo "Пользователь не найден";
+    exit();
+}
+setcookie('user',$user['name'],time()+3600,"/");
+$mysql->close();
+header('Location:/');
+?>
---
+++ validation/check.php
@@ -0,0 +1,27 @@
+<?php
+$email=filter_var(trim($_POST['email']),
+FILTER_SANITIZE_STRING);
+$name=filter_var(trim($_POST['nickname']),
+    FILTER_SANITIZE_STRING);
+$pass=filter_var(trim($_POST['pass']),
+    FILTER_SANITIZE_STRING);
+
+if(mb_strlen($name)<5 || mb_strlen($name)>90){
+    echo "Недопустимая длина никнейма";
+    exit;
+}
+if(mb_strlen($email)<5 || mb_strlen($email)>99){
+    echo "Недопустимая длина почты";
+    exit;
+}
+if(mb_strlen($pass)<8 || mb_strlen($pass)>15){
+    echo "Недопустимая длина пароля (от 8 до 15 символов)";
+    exit;
+}
+$pass=md5($pass."imperium4");
+$mysql=new mysqli('localhost','root','','project');
+$mysql->query("INSERT INTO  `users` (`mail`,`nickname`,`pass`)
+VALUES('$email','$name','$pass')");
+$mysql->close();
+header('Location:/');
+?>
