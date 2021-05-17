51e1218a32ea66d18b8b47beda9df691e63556de
ko1z-se-2015 <201402@astanait.edu.kz>
2021-05-17 18:16:17 +0300

    books

--- validation/exit.php
+++ validation/exit.php
@@ -1,6 +1,5 @@
 <?php
-unset($_COOKIE['name']);
-setcookie('name','', time()-3600,'/');
+unset($_COOKIE['user']);
+setcookie('user','', time()-3600,'/');
 header("location: http://localhost:63342/books/main_page.php?_ijt=kq22jsabbm148d8oot6lr40h1b");
-
 ?>
