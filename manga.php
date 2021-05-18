b8379fd684a8f9c2e89d2290662e5cd6f3810096
elhanz <elhanz@mail.ru>
2021-05-18 10:19:07 +0600

    little fix

--- manga.php
+++ manga.php
@@ -2,7 +2,7 @@
 $mysql = new mysqli('localhost', 'root', '', 'project');
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="eng">
 <head>
     <link href="manga_css.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -66,108 +66,7 @@ include "DB/connnect.php";
             </div>
         </div>
         <div class="books">
-            <!--            <div class="row_books">-->
-            <!--                <div class="book">-->
-            <!--                    <div class="img_manga" id="manga1"></div>-->
-            <!--                    <div class="description">-->
-            <!--                        <div class="title_product">Маг, который смог</div>-->
-            <!--                        <div class="des">Жанр:приключения, комедия</div>-->
-            <!--                        <div class="price">Цена: 2000тенге</div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--                <div class="book">-->
-            <!--                    <div class="img_manga" id="manga2"></div>-->
-            <!--                    <div class="description">-->
-            <!--                        <div class="title_product">Елхан, человек из бездны</div>-->
-            <!--                        <div class="des">Жанр:драма, фантастика, насилие</div>-->
-            <!--                        <div class="price">Цена: 5000тенге</div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--                <div class="book">-->
-            <!--                    <div class="img_manga" id="manga3"></div>-->
-            <!--                    <div class="description">-->
-            <!--                        <div class="title_product">Отбитая группа SE</div>-->
-            <!--                        <div class="des">Жанр:комедия,посевдневность</div>-->
-            <!--                        <div class="price">Цена: 2500тенге</div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--                <div class="book">-->
-            <!--                    <div class="img_manga" id="manga4"></div>-->
-            <!--                    <div class="description">-->
-            <!--                        <div class="title_product">Данчик - целитель, история СЕшника</div>-->
-            <!--                        <div class="des">Жанр:ужастик,войны,насилие</div>-->
-            <!--                        <div class="price">Цена: 10000тенге</div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--            </div>-->
-            <!--            <div class="row_books">-->
-            <!--                <div class="book">-->
-            <!--                    <div class="img_manga" id="manga5"></div>-->
-            <!--                    <div class="description">-->
-            <!--                        <div class="title_product">Боксер - Талыг</div>-->
-            <!--                        <div class="des">Жанр: спорт, комедия, драки</div>-->
-            <!--                        <div class="price">Цена: 2500тенге</div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--                <div class="book">-->
-            <!--                    <div class="img_manga" id="manga6"></div>-->
-            <!--                    <div class="description">-->
-            <!--                        <div class="title_product">Амир!Это мастер пикапа!</div>-->
-            <!--                        <div class="des">Жанр: комедия, романтика</div>-->
-            <!--                        <div class="price">Цена: 3000тенге</div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--                <div class="book">-->
-            <!--                    <div class="img_manga" id="manga7"></div>-->
-            <!--                    <div class="description">-->
-            <!--                        <div class="title_product">Суетолог</div>-->
-            <!--                        <div class="des">Жанр: ужастик, насилие</div>-->
-            <!--                        <div class="price">Цена: 9000тенге</div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--                <div class="book">-->
-            <!--                    <div class="img_manga" id="manga8"></div>-->
-            <!--                    <div class="description">-->
-            <!--                        <div class="title_product">Программист, попавший в иной мир</div>-->
-            <!--                        <div class="des">Жанр: приключение, фантастика</div>-->
-            <!--                        <div class="price">Цена: 5000тенге</div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--            </div>-->
-            <!--            <div class="row_books">-->
-            <!--                <div class="book">-->
-            <!--                    <div class="img_manga" id="manga9"></div>-->
-            <!--                    <div class="description">-->
-            <!--                        <div class="title_product">Демон из Венеры</div>-->
-            <!--                        <div class="des">Жанр:триллер, детектив</div>-->
-            <!--                        <div class="price">Цена: 500тенге</div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--                <div class="book">-->
-            <!--                    <div class="img_manga" id="manga10"></div>-->
-            <!--                    <div class="description">-->
-            <!--                        <div class="title_product">Юху!Спорт!</div>-->
-            <!--                        <div class="des">Жанр:комедия, спорт, гарем</div>-->
-            <!--                        <div class="price">Цена: 3000тенге</div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--                <div class="book">-->
-            <!--                    <div class="img_manga" id="manga11"></div>-->
-            <!--                    <div class="description">-->
-            <!--                        <div class="title_product">Если бы ты был как я</div>-->
-            <!--                        <div class="des">Жанр: романтика, драма</div>-->
-            <!--                        <div class="price">Цена: 6000тенге</div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--                <div class="book">-->
-            <!--                    <div class="img_manga" id="manga12"></div>-->
-            <!--                    <div class="description">-->
-            <!--                        <div class="title_product">Да, я повар.И что?</div>-->
-            <!--                        <div class="des">Жанр: кулинария, драки</div>-->
-            <!--                        <div class="price">Цена: 2000тенге</div>-->
-            <!--                    </div>-->
-            <!--                </div>-->
-            <!--            </div>-->
+
             <div class="row_books">
                 <?php
                 $book = mysqli_query($mysql, "SELECT * FROM `books` WHERE `categery_id` = 1 LIMIT 0,4");
